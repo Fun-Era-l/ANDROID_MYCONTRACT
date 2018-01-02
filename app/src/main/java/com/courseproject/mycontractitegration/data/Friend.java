e366234de2ab859bca45e8e93aec057884376762
Luwenjie1997 <27827478@qq.com>
2018-01-02 09:28:53 +0800

    friendmanage

---
+++ app/src/main/java/com/courseproject/mycontractitegration/data/Friend.java
@@ -0,0 +1,10 @@
+package com.courseproject.mycontractitegration.data;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 27827 on 2017/12/31.
+ */
+
+public class Friend extends DataSupport {
+}
---
+++ app/src/main/java/com/courseproject/mycontractitegration/friendManage/FriendModel.java
@@ -0,0 +1,8 @@
+package com.courseproject.mycontractitegration.friendManage;
+
+/**
+ * Created by 27827 on 2017/12/31.
+ */
+
+public class FriendModel {
+}
